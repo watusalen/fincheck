f0d555b53c9b0ef964c42efc27f59c157cb9e315
Cicero Andrade <cicero123cicero321@gmail.com>
2025-07-08 21:10:14 -0300

    feat(view): adicionar o arquivo index.ts para centralizar as exportações das Views

---
+++ src/view/index.ts
@@ -0,0 +1,8 @@
+// Exportações centralizadas das views
+export { MainView } from './MainView';
+export { RegisterView } from './RegisterView';
+export { DashboardView } from './DashboardView';
+export { TransactionView } from './TransactionView';
+export { ChartView } from './ChartView';
+export { HistoryView } from './HistoryView';
+export { CreateView } from './CreateView';
